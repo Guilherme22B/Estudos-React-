654aacecdc0e1e2618bf9a889334c4da4bdf3522
GuilhermeSilvaM <guimenezes125@gmail.com>
2023-08-04 17:16:58 -0300

    aula 03

--- meu-projeto/meu-projeto/src/App.js
+++ meu-projeto/meu-projeto/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 
 function App() {
+  
+  var nome = 'Guilherme'
+
+  function sum(a,b){
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>meu primeiro app</p>
+
+
+
+      <h2>Alterando o jsx</h2>
+
+      <p>olá, {nome}</p>
+
+      <p>soma: {sum(3 , 5)} </p>
+
+      <img src={url} alt="Miha imagem"/>
     </div>
   );
 }
