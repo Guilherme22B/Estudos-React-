4aacf68a64042b00ee988edc38b2ce9f060220b8
GuilhermeSilvaM <guimenezes125@gmail.com>
2024-02-17 21:43:25 -0300

    .

--- meu-projeto/meu-projeto/src/components/Form.js
+++ meu-projeto/meu-projeto/src/components/Form.js
@@ -1,8 +1,18 @@
+import {useState} from 'react';
+import style from './Form.module.css';
+
 function Form(){
     function cadastrarUsuario(e){
         e.preventDefault()
-        console.log("cadastrou o usuario");
+
+
+        console.log(name)
+        console.log(passoword);
     }
+
+
+    const[name, setName] = useState()
+    const[passoword, setPassword] = useState()
 
 
     return(
@@ -12,9 +22,15 @@ function Form(){
             <h1>Meu Cadastro: </h1>
 
             <form onSubmit={cadastrarUsuario}>
+
+
                 <div>
-                    <p>pq imput não funciona?</p>
-                    <input  type="text"placeholder="digite o seu nome" ></input>
+                    <label htmlFor="name"> Nome: </label>
+                    <input  type="text"placeholder="digite o seu nome" id="name" name="name" ></input>
+                </div>
+                <div>
+                    <label htmlFor="passoword">Senha: </label>
+                    <input  type="password"placeholder="digite a sua senha" id="password" name="password" ></input>
                 </div>
                 <div>
                     <input type="submit" value="cadastrar"></input>
